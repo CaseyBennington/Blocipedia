f18f03cdb25439d0cdd5a8f026cea18570d7963e
Casey Bennington <cbenning@vt.edu>
2016-05-23 23:40:36 -0700

    Complete UserStory2

---
+++ spec/features/new_user_signups_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.feature "NewUserSignups", type: :feature do
+  it "should" do
+    
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { create(:user) }
+
+  # Shoulda tests for email
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+  it { is_expected.to validate_length_of(:email).is_at_least(3) }
+  it { is_expected.to allow_value('user@bloccit.com').for(:email) }
+
+  # Shoulda tests for password
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to have_secure_password }
+  it { is_expected.to validate_length_of(:password).is_at_least(6) }
+
+  describe 'attributes' do
+    it 'should have name and email attributes' do
+      expect(user).to have_attributes(name: user.name, email: user.email)
+    end
+
+    it 'responds to role' do
+      expect(user).to respond_to(:role)
+    end
+
+    it 'responds to admin?' do
+      expect(user).to respond_to(:admin?)
+    end
+
+    it 'responds to member?' do
+      expect(user).to respond_to(:member?)
+    end
+  end
+
+  describe 'roles' do
+    it 'is member by default' do
+      expect(user.role).to eq('member')
+    end
+
+    context 'member user' do
+      it 'returns true for #member?' do
+        expect(user.member?).to be_truthy
+      end
+
+      it 'returns false for #admin?' do
+        expect(user.admin?).to be_falsey
+      end
+    end
+
+    context 'admin user' do
+      before do
+        user.admin!
+      end
+
+      it 'returns false for #member?' do
+        expect(user.member?).to be_falsey
+      end
+
+      it 'returns true for #admin?' do
+        expect(user.admin?).to be_truthy
+      end
+    end
+  end
+
+  describe 'invalid user' do
+    let(:user_with_invalid_name) { build(:user, name: '') }
+    let(:user_with_invalid_email) { build(:user, email: '') }
+
+    it 'should be an invalid user due to blank name' do
+      expect(user_with_invalid_name).to_not be_valid
+    end
+
+    it 'should be an invalid user due to blank email' do
+      expect(user_with_invalid_email).to_not be_valid
+    end
+  end
+
+  describe '.avatar_url' do
+    let(:known_user) { create(:user, email: 'blochead@bloc.io') }
+
+    it 'returns the proper Gravatar url for a known email entity' do
+      expected_gravatar = 'http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48'
+      expect(known_user.avatar_url(48)).to eq(expected_gravatar)
+    end
+  end
+end
