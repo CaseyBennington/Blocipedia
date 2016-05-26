36feb48aecf4f2bc75440503c3ff6b586f02b082
Casey Bennington <cbenning@vt.edu>
2016-05-25 17:22:19 -0700

    Partial complete UserStory3

---
+++ app/models/wiki.rb
@@ -0,0 +1,10 @@
+class Wiki < ActiveRecord::Base
+  belongs_to :user
+
+  default_scope { order('title DESC') }
+  scope :visible_to, -> (user) { user ? all : where(private: false) }
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :user, presence: true
+end
