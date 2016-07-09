1d60089a5418b472369e0559ea4824f3a1206cd6
Casey Bennington <cbenning@vt.edu>
2016-07-09 01:27:14 -0700

    Heroku asset sserving error potential fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # Fix to ensure Heroku serves assets
+  config.serve_static_files = true
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
