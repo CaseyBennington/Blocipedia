644dcbd28b76ac9a48b6151e8697a7703dbb4a6e
Casey Bennington <cbenning@vt.edu>
2016-11-01 14:55:08 -0700

    heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.serve_static_files = true
-  config.assets.compile = true
+  # config.assets.compile = true
+  config.assets.precompile += %w( .svg .eot .woff .tff .png .jpg .gif)
   config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
